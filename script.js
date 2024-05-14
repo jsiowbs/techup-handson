2dc1e710b7b4b60d37977077829d97e74f2669c6
jsiowbs <48074920+jsiowbs@users.noreply.github.com>
2024-05-14 04:30:12 +0000

    javascript trialling

---
+++ script.js
@@ -0,0 +1,32 @@
+var age = 65
+console.log(age)
+
+var username = "jsiow"
+var password = "techup"
+
+var isLoggedIn = false
+
+var blog_posts = ["today was a good day", "the sky is blue", "hello world and nonsense"]
+
+console.log(username);
+console.log(password);
+console.log(isLoggedIn);
+console.log(blog_posts);
+
+var isMale = false;
+
+if (isMale == true){
+    console.log("you are male");
+}
+else{
+    console.log("you are not male");
+}
+
+while (age < 70) {
+    console.log("Your age is " + age );
+    age = age + 1;
+}
+
+function sayHello(){
+    console.log("Hello Everyone");
+}
